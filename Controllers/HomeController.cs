1250a2989dc1974488cdf290251c9a04007392d9
Bianca Marian <bia.nico47@yahoo.com>
2021-10-19 20:41:24 +0300

    Lab4

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,16 +6,19 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Marian_Bianca_Lab2.Data;
+using Marian_Bianca_Lab2.Models.LibraryViewModels;
 
 namespace Marian_Bianca_Lab2.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly LibraryContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(LibraryContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -27,11 +30,24 @@ namespace Marian_Bianca_Lab2.Controllers
         {
             return View();
         }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public async Task<ActionResult> Statistics()
+        {
+            IQueryable<OrderGroup> data =
+                                          from order in _context.Orders
+                                          group order by order.OrderDate into dateGroup
+                                          select new OrderGroup()
+                                          {
+                                              OrderDate = dateGroup.Key,
+                                              BookCount = dateGroup.Count()
+                                          };
+            return View(await data.AsNoTracking().ToListAsync());
+        }
+
     }
 }
