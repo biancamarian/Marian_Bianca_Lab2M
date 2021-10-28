94522104f30813bebe32af0a12b02c3da1d480f6
Bianca Marian <bia.nico47@yahoo.com>
2021-10-28 18:09:01 +0300

    Lab4Suplimentar

---
+++ Models/CustomerViewModels/CustomerGroup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Marian_Bianca_Lab2.Models.CustomerViewModels
+{
+    public class CustomerGroup
+    {
+        public string CustomerName { get; set; }
+        public int BookCount { get; set; }
+    }
+}
